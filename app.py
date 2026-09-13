75d10f4832853dcbd556253a0fba8edadfc869ac
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-13 14:39:25 -0500

    basic loop done

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from help import help
 
 while True:
-    command = input("> ")
+    command = input("> ").lower()
 
     if command == "help":
         print(help())
-    
+
+    if command == "exit":
+        exit()
+
+    print("Unknown Command")
