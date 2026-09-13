639cad29682096ba3333ba61a8faaa513a0c3e04
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-13 14:17:56 -0500

    small feature doen so i will try to speedrun all the basic fetures

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from help import help
+
 while True:
     command = input("> ")
 
     if command == "help":
+        print(help())
     
--- help.py
+++ help.py
@@ -1,1 +1,9 @@
 # i am planning to add a lot of features so help is required so i put it here instead of confuesing me in the app.py
+
+
+features = """
+'help' - to get this block.
+"""
+
+def help():
+    return features.strip()
